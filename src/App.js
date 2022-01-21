b60e239288881f07daf18927a57020590a022419
Craig Miller <cmiller@cs.depaul.edu>
2022-01-21 11:21:41 -0600

    minor change to message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React again
+          Learn React again, yet another version...
         </a>
       </header>
     </div>
